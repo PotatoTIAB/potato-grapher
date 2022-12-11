c794d9880e7ea8348358b5b27fd6228e5d3621f8
PotatoTIAB <serkanicoz2005@gmail.com>
2022-12-11 19:38:10 +0300

    canvas do stuff.

--- window.py
+++ window.py
@@ -1,7 +1,8 @@
-import tkinter as tk
+from tkinter import *
 
-root = tk.Tk()
-canvas = tk.Canvas()
+root = Tk()
+canvas = Canvas(root, width=500, height=400, background="#eeeeee")
+canvas.bind("<Button-1>", lambda e: canvas.create_text(e.x, e.y, text="owo"))
 
 canvas.pack()
 root.mainloop()
