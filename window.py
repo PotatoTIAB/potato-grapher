ec9bb8b6b1f8ff492e789b287bf12b0de2074fed
PotatoTIAB <serkanicoz2005@gmail.com>
2022-12-11 19:57:34 +0300

    paint 2.0

--- window.py
+++ window.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+
+
 class Window(Tk):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
@@ -7,10 +9,25 @@ class Window(Tk):
 
 		self.canvas.pack()
 
+
+
 class Plane(Canvas):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.bind("<Button-1>", lambda e: self.create_text(e.x, e.y, text="owo"))
+
+		self.bind("<Button-1>", self.on_click)
+		self.bind("<B1-Motion>", self.on_motion)
+		self.x = 0
+		self.y = 0
+	
+
+	def on_click(self, event: Event):
+		self.x, self.y = event.x, event.y
+
+
+	def on_motion(self, event: Event):
+		self.create_line(self.x, self.y, event.x, event.y)
+		self.x, self.y = event.x, event.y
 
 
 root = Window()
